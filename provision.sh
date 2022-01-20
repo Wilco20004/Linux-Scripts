b02154e223ea621a671a17c1133606c1b49c2069
Willem Coetzee <wilco@peachss.co.za>
2022-01-20 20:02:14 +0200

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -19,25 +19,6 @@ docker run -d -p 8000:8000 -p 9443:9443 --name portainer \
     -v portainer_data:/data \
     cr.portainer.io/portainer/portainer-ce:2.9.3
 
-sudo  apt-get  install nginx -y
-sudo apt install pptp-linux -y
-cd /etc/ppp/peers
-touch PPTP
-echo "pty "pptp Your_Server_IP --nolaunchpppd --debug" \
-name Your_Username \
-password Your_Password \
-remotename PPTP  \
-require-mppe-128 \
-require-mschap-v2 \
-refuse-eap \
-refuse-pap \
-refuse-chap \
-refuse-mschap \
-noauth \
-debug \
-persist \
-maxfail 0  \
-defaultroute" > PPTP
 
 sudo mkdir /var/docker
 sudo chmod 0777 /var/docker
@@ -48,9 +29,3 @@ sudo docker run -it --name code-server -p 9111:8080 \
   -u "$(id -u):$(id -g)" \
   -e "DOCKER_USER=$USER" \
   codercom/code-server:latest
-
-
-sudo ufw allow 80
-sudo ufw allow 443
-sudo apt install letsencrypt
-apt install python3-certbot-nginx  -y
